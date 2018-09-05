0796ce0e79a4ce7b16e09fb514f445bcf487fac8
Ali Mahmudov <maxali946@gmail.com>
2018-09-05 10:32:07 +0400

    80%

--- ConsoleApplication.Tests/OrdersTests.cs
+++ ConsoleApplication.Tests/OrdersTests.cs
@@ -1,6 +1,7 @@
 ﻿using ConsoleApplication.Models;
 using ConsoleApplication.Repositories;
 using ConsoleApplication.Services;
+using Moq;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -53,6 +54,15 @@ namespace ConsoleApplication.Tests
 
     public class OrdersTests
     {
+        private List<Order> ordersList = new List<Order>
+        {
+            new Order("as", 1, "Al") { Id = 1, Delivered = true },
+                new Order("asdf", 5, "Alas") {Id = 2, Delivered = false },
+                new Order("as", 3, "Afl") {Id = 3, Delivered = true },
+                new Order("zxcv", 1, "Afzsl") {Id = 4, Delivered = false },
+                new Order("qwer", 2, "AflAad") {Id = 5, Delivered = true }
+        };
+
         [Fact]
         public void Test_GetDeliveredOrders_For_Delivered_prop()
         {
@@ -67,11 +77,28 @@ namespace ConsoleApplication.Tests
             Assert.All(orders, o => Assert.True(o.Delivered));
         }
 
+        //[Fact]
+        //public void Test_GetOrders_For_Buyer_Is_Not_Excists_prop()
+        //{
+        //    //Arrange
+        //    var repo = new FakeRepo();
+        //    var service = new OrdersService(repo);
+
+        //    //Act
+        //    var orders = service.GetOrdersForBuyer("qwe");
+
+        //    //Assert
+        //    Assert.Empty(orders);
+        //}
+
         [Fact]
         public void Test_GetOrders_For_Buyer_Is_Not_Excists_prop()
         {
             //Arrange
-            var repo = new FakeRepo();
+            var mock = new Mock<IRepository>();
+            mock.Setup(m => m.GetAllOrders()).Returns(ordersList);
+            var repo = mock.Object;
+
             var service = new OrdersService(repo);
 
             //Act
@@ -90,6 +117,23 @@ namespace ConsoleApplication.Tests
 
             //Act
             service.DeliverOrder(2);
+
+            //Assert
+            Assert.True(true);
+        }
+
+        [Theory]
+        [InlineData(1)]
+        [InlineData(2)]
+        [InlineData(3)]
+        public void Test_Deliver_Order_with_Param(int id)
+        {
+            //Arrange
+            var repo = new FakeRepo();
+            var service = new OrdersService(repo);
+
+            //Act
+            service.DeliverOrder(id);
 
             //Assert
             Assert.True(true);
